94093f98c16a97232d71b41e4f17a7b7276b2c88
devedbox <devedbox@gmail.com>
2017-07-05 10:54:52 +0800

    Updated sample codes.

--- AxReminder/Classes/AppDelegates/AppDelegateHierarchy.swift
+++ AxReminder/Classes/AppDelegates/AppDelegateHierarchy.swift
@@ -30,15 +30,51 @@ extension AppDelegate {
         tabNavigationController?.addViewController(home)
         tabNavigationController?.addViewController(category)
         
-        let settingsItem = TabNavigationTitleActionItem(title: "设置", target: self, selector: #selector(_handleShowingSettingsViewController(_:)))
+        let settingsItem = TabNavigationTitleActionItem(title: "显示", target: self, selector: #selector(_handleShowingSettingsViewController(_:)))
         settingsItem.tintColor = UIColor.application.blue
+        let hidingItem = TabNavigationTitleActionItem(title: "隐藏", target: self, selector: #selector(_handleHidingSettingsViewController(_:)))
+        hidingItem.tintColor = UIColor.application.blue
         tabNavigationController?.tabNavigationBar.addNavigationTitleItem(settingsItem)
+        tabNavigationController?.tabNavigationBar.addNavigationTitleItem(hidingItem)
         
         tabNavigationController?.tabNavigationBar.setSelectedTitle(at: 0, animated: false)
     }
     
     @objc
     private func _handleShowingSettingsViewController(_ sender: UIButton) {
-        print("Did click settings navigation title action item.")
+        let titleItem0 = TabNavigationTitleItem(title: "呵呵")
+        let titleItem1 = TabNavigationTitleItem(title: "哈哈")
+        let titleItems = [titleItem0, titleItem1]
+        self.tabNavigationController?.tabNavigationBar.setNavigationTitleItems(titleItems, animated: true) /* { () -> (Bool, [TabNavigationTitleActionItem]?) in
+            return (true, nil)
+        } */
+        self.tabNavigationController?.tabNavigationBar.showNavigationBackItem(true)
+        /*
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { 
+            let titleItem0 = TabNavigationTitleItem(title: "主页")
+            let titleItem1 = TabNavigationTitleItem(title: "分类")
+            let titleItems = [titleItem0, titleItem1]
+            
+            let settingsItem = TabNavigationTitleActionItem(title: "显示", target: self, selector: #selector(self._handleShowingSettingsViewController(_:)))
+            settingsItem.tintColor = UIColor.application.blue
+            let hidingItem = TabNavigationTitleActionItem(title: "隐藏", target: self, selector: #selector(self._handleHidingSettingsViewController(_:)))
+            hidingItem.tintColor = UIColor.application.blue
+            let actions = [settingsItem, hidingItem]
+            
+            self.tabNavigationController?.tabNavigationBar.setNavigationTitleItems(titleItems, animated: true) { () -> (Bool, [TabNavigationTitleActionItem]?) in
+                return (false, actions)
+            }
+            
+            self.tabNavigationController?.tabNavigationBar.hideNavigationBackItem(true)
+        } */
+    }
+    @objc
+    private func _handleHidingSettingsViewController(_ sender: UIButton) {
+        let titleItem0 = TabNavigationTitleItem(title: "主页")
+        let titleItem1 = TabNavigationTitleItem(title: "分类") 
+        let titleItems = [titleItem0, titleItem1]
+        self.tabNavigationController?.tabNavigationBar.setNavigationTitleItems(titleItems, animated: true)
+
+        self.tabNavigationController?.tabNavigationBar.hideNavigationBackItem(true)
     }
 }
