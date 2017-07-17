8dd0280ef3d0cb86b96eec8bc8d7755a08e084dc
devedbox <devedbox@gmail.com>
2017-07-17 17:23:34 +0800

    Added AxTabNavigationController.

---
+++ AxReminder/Classes/Generals/ViewControllers/AxTabNavigationController.swift
@@ -0,0 +1,24 @@
+//
+//  AxTabNavigationController.swift
+//  AxReminder
+//
+//  Created by devedbox on 2017/7/17.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class AxTabNavigationController: TabNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(hex: "F9F9F9")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
