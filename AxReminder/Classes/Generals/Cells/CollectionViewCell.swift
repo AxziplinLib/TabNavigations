8860ad5d1c0e48a6bc487968a2a3dd3916dfdbbf
devedbox <devedbox@gmail.com>
2017-07-25 17:45:56 +0800

    Added CollectionViewCell class.

---
+++ AxReminder/Classes/Generals/Cells/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  AxReminder
+//
+//  Created by devedbox on 2017/7/25.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    dynamic
+    public class var reusedIdentifier: String { return "_CollectionViewCell" }
+}
