c0b79f3e82f10142e4758a9ec6c3c7ffb84c57e7
devedbox <devedbox@gmail.com>
2017-07-25 17:50:56 +0800

    Added CollectionReusableView.

---
+++ AxReminder/Classes/Generals/Views/CollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionReusableView.swift
+//  AxReminder
+//
+//  Created by devedbox on 2017/7/25.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+}
