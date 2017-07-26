b4339bd6ee69f039ae5ff0a006b7a8fae4218464
devedbox <devedbox@gmail.com>
2017-07-26 09:21:23 +0800

    Added ItemAddingCollectionReusableView class.

---
+++ AxReminder/Classes/Modules/Item/Views/ItemAddingCollectionReusableView.swift
@@ -0,0 +1,21 @@
+//
+//  ItemAddingCollectionReusableView.swift
+//  AxReminder
+//
+//  Created by devedbox on 2017/7/26.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class ItemAddingCollectionReusableView: CollectionReusableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
