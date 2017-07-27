cc30de7302982dae2895ab81c1ee332a78992efc
devedbox <devedbox@gmail.com>
2017-07-27 15:26:06 +0800

    Added TabNavigationImagePickerController image picker.

---
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigationImagePickerController.swift
@@ -0,0 +1,23 @@
+//
+//  TabNavigationImagePickerController.swift
+//  AxReminder
+//
+//  Created by devedbox on 2017/7/27.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class TabNavigationImagePickerController: TabNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
