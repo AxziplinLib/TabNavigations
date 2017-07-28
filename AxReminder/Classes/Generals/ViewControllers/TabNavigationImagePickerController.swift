44aa948cd15ebc8884dd971b656ce824adf63460
devedbox <devedbox@gmail.com>
2017-07-28 14:21:24 +0800

    Finished image content displaying,

--- AxReminder/Classes/Generals/ViewControllers/TabNavigationImagePickerController.swift
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigationImagePickerController.swift
@@ -7,17 +7,233 @@
 //
 
 import UIKit
+import Photos
+import AXPracticalHUD
 
 class TabNavigationImagePickerController: TabNavigationController {
-
+    fileprivate var _photoAssetCollections: [PHAssetCollection] = { () -> [PHAssetCollection] in
+        AXPracticalHUD.shared().showSimple(in: UIApplication.shared.keyWindow!)
+        let results = TabNavigationImagePickerController.generatePhotoAssetCollections()
+        let assets = results.objects(at: IndexSet(integersIn: 0..<results.count)).filter{ PHAsset.fetchAssets(in: $0, options: nil).count > 0 }.sorted { PHAsset.fetchAssets(in: $0, options: nil).count > PHAsset.fetchAssets(in: $1, options: nil).count
+        }
+        AXPracticalHUD.shared().hide(true, afterDelay: 0.5, completion: nil)
+        return assets
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        _initializer()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        _initializer()
+    }
+    
+    private func _initializer() {
+        // Enumerate the asset collections.
+        if !shouldIncludeHiddenAssets {
+            _photoAssetCollections = _photoAssetCollections.filter{ $0.assetCollectionSubtype != .smartAlbumAllHidden }
+        }
+        for assetCollection in _photoAssetCollections {
+            // Add image collection view controllers.
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .vertical
+            let assetViewController = _AssetCollectionViewController(collectionViewLayout: flowLayout, photoAlbum: assetCollection)
+            self.addViewController(assetViewController)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(colorLiteralRed: 0.976, green: 0.976, blue: 0.976, alpha: 1.0)
+        tabNavigationBar.isTranslucent = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+// MARK: - Public.
+
+extension TabNavigationImagePickerController {
+    open var shouldIncludeHiddenAssets: Bool { return false }
+    
+    public func requestAuthorizationIfNeeded() {
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .authorized: break
+        default:
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                // Handle authorization status.
+                print("PHPhotoLibrary.requestAuthorization: \(status)")
+            })
+        }
+    }
+    
+    open class func generatePhotoAssetCollections() -> PHFetchResult<PHAssetCollection> {
+        let option = PHFetchOptions()
+        option.includeHiddenAssets = true
+        option.includeAllBurstAssets = false
+        // option.predicate = NSPredicate(format: "")
+        return PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .any, options: option)
+    }
+}
+
+fileprivate class _AssetCollectionViewController: UICollectionViewController {
+    var _photoAssetCollection: PHAssetCollection!
+    var _photoAssets: PHFetchResult<PHAsset>!
+    
+    convenience init(collectionViewLayout layout: UICollectionViewLayout, photoAlbum assetCollection: PHAssetCollection) {
+        self.init(collectionViewLayout: layout)
+        _photoAssetCollection = assetCollection
+        
+        // Fetch assets from asset collection.
+        let option = PHFetchOptions()
+        option.sortDescriptors = [NSSortDescriptor(key: "modificationDate", ascending: false)]
+        _photoAssets = PHAsset.fetchAssets(in: _photoAssetCollection, options: option)
+    }
+    
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+        _initializer()
+    }
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        _initializer()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        _initializer()
+    }
+    
+    private func _initializer() {
+        
+    }
+    
+    // Life cycle.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView!.backgroundColor = .clear
+        collectionView!.alwaysBounceVertical = true
+        
+        setTabNavigationTitle(["title": _photoAssetCollection.localizedTitle ?? "", "range": 0..<2])
+        let cancel = TabNavigationItem(title: "取消", target: self, selector: #selector(_handleCancelAction(_:)))
+        setTabNavigationItems([cancel])
+        // Register asset collection cell.
+        collectionView!.register(_AssetCollectionCell.self, forCellWithReuseIdentifier: String(describing: _AssetCollectionCell.self))
+        
+        collectionView!.contentInset = UIEdgeInsets(top: tabNavigationController!.tabNavigationBar.bounds.height, left: 0.0, bottom: 0.0, right: 0.0)
+        collectionView!.scrollIndicatorInsets = collectionView!.contentInset
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+}
+
+// MARK: Actions.
+
+extension _AssetCollectionViewController {
+    @objc
+    fileprivate func _handleCancelAction(_ sender: UIButton) {
+        self.tabNavigationController!.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension _AssetCollectionViewController {
+    override var layoutInsets: UIEdgeInsets { return .zero }
+}
+
+extension _AssetCollectionViewController {
+    fileprivate var _DefaultCollectionSectionInset: CGFloat { return 1.0 }
+    fileprivate var _DefaultCollectionItemPadding: CGFloat { return 2.0 }
+    fileprivate var _DefaultCollectionItemColumns: CGFloat { return 4.0 }
+}
+extension _AssetCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Display 4 columns.
+        let size_s = (collectionView.bounds.width - _DefaultCollectionSectionInset * 2.0 - _DefaultCollectionItemPadding * (_DefaultCollectionItemColumns - 1.0)) / _DefaultCollectionItemColumns
+        return CGSize(width: size_s, height: size_s)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: _DefaultCollectionSectionInset, left: _DefaultCollectionSectionInset, bottom: _DefaultCollectionSectionInset, right: _DefaultCollectionSectionInset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return _DefaultCollectionItemPadding
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return _DefaultCollectionItemPadding
+    }
+}
+
+// MARK: - CollectionViewController Delegate And DataSource Supporting.
+
+extension _AssetCollectionViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return _photoAssets.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: _AssetCollectionCell.self), for: indexPath) as! _AssetCollectionCell
+        
+        let asset = _photoAssets.object(at: indexPath.item)
+        let option = PHImageRequestOptions()
+        option.isNetworkAccessAllowed = true
+        option.deliveryMode = .opportunistic
+        option.resizeMode = .none
+        option.isSynchronous = false
+        
+        let size_ta = self.collectionView(collectionView, layout: collectionView.collectionViewLayout, sizeForItemAt: indexPath)
+        PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: size_ta.width * UIScreen.main.scale, height: size_ta.height * UIScreen.main.scale), contentMode: .aspectFill, options: option) { (image, info) in
+            cell.imageView.image = image
+        }
+        
+        return cell
+    }
+}
+
+// MARK: _AssetCollectionViewController
+
+extension _AssetCollectionViewController {
+    class _AssetCollectionCell: UICollectionViewCell {
+        let imageView: UIImageView = UIImageView()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            _initializer()
+        }
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            _initializer()
+        }
+        
+        private func _initializer() {
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(imageView)
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+            
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+        }
+        
+        override func prepareForReuse() {
+            super.prepareForReuse()
+            
+            imageView.image = nil
+        }
     }
 }
