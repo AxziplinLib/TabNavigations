e2c02e31bf40d3729fe94004fe37613f1e70fd90
devedbox <862099730@qq.com>
2017-08-05 17:48:34 +0800

    Updated source files.

---
+++ AxReminder/Classes/Modules/Item/ViewControllers/ItemAddingCollectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ItemAddingCollectionViewController.swift
+//  AxReminder
+//
+//  Created by devedbox on 2017/7/26.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class ItemAddingCollectionViewController: CollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
