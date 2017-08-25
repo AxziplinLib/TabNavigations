db5566c47a6e88908fa2d784e3b4b010a2cede53
aixing-hb <aixing@hydsoft.com>
2017-08-25 14:21:14 +0800

    Renamed and regrouped files.

---
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigations/ImagePicker/ImagePicker.swift
@@ -0,0 +1,232 @@
+//
+//  ImagePicker.swift
+//  TabNavigations/ImagePicker
+//
+//  Created by devedbox on 2017/7/27.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+import Photos
+import AVFoundation
+
+// MARK: - TabNavigationImagePickerController.
+
+extension TabNavigationImagePickerController {
+    public typealias ImagesResultHandler = (([UIImage]) -> Void)
+}
+
+@objc public protocol TabNavigationImagePickerControllerDelegate {
+    @objc
+    optional func imagePickerWillBeginFetchingAssetCollection(_ imagePicker: TabNavigationImagePickerController)
+    @objc
+    optional func imagePickerDidFinishFetchingAssetCollection(_ imagePicker: TabNavigationImagePickerController)
+}
+
+open class TabNavigationImagePickerController: TabNavigationController {
+    fileprivate lazy var _photoAssetCollections: [PHAssetCollection] = { () -> [PHAssetCollection] in
+        let results = TabNavigationImagePickerController.generatePhotoAssetCollections()
+        let option = PHFetchOptions()
+        option.predicate = NSPredicate(format: "mediaType == \(PHAssetMediaType.image.rawValue)")
+        let assets = results.objects(at: IndexSet(integersIn: 0..<results.count)).filter{ PHAsset.fetchAssets(in: $0, options: option).count > 0 }.sorted { PHAsset.fetchAssets(in: $0, options: option).count > PHAsset.fetchAssets(in: $1, options: option).count
+        }
+        return assets
+    }()
+    // ---
+    internal    var _selectedIndexPathsOfAssets: [String: [IndexPath]] = [:]
+    open        var allowedSelectionCounts     : Int = 9
+    open weak   var delegate                   : TabNavigationImagePickerControllerDelegate?
+    // ---
+    fileprivate var _captureSession            : AVCaptureSession!
+    fileprivate let _captureSessionQueue       = DispatchQueue(label: "com.imagepicker.session")
+    internal    var _captureDeviceInput        : AVCaptureDeviceInput!
+    fileprivate let _captureDisplayOutput      = AVCaptureVideoDataOutput()
+    internal    var _captureDisplayViews       : Set<CameraViewController.CaptureVideoDisplayView> = []
+    internal    let _captureDisplayQueue       = DispatchQueue(label: "com.imagepicker.display.render")
+    internal    var _captureVideoPreviewView   : CameraViewController.CaptureVideoPreviewView!
+    // ---
+    fileprivate var _lastSampleBuffer          : CMSampleBuffer!
+    // ---
+    fileprivate var imagesResult               : ImagesResultHandler?
+    
+    public init(delegate: TabNavigationImagePickerControllerDelegate? = nil, imagesResult: ImagesResultHandler? = nil) {
+        super.init(nibName: nil, bundle: nil)
+        self.delegate = delegate
+        self.imagesResult = imagesResult
+        _initializer()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        _initializer()
+    }
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        _initializer()
+    }
+    
+    private func _initializer() {
+        isTabNavigationItemsUpdatingDisabledInRootViewControllers = true
+        // Enumerate the asset collections.
+        willBeginFetchingAssetCollection()
+        _initSession()
+        if !shouldIncludeHiddenAssets {
+            _photoAssetCollections = _photoAssetCollections.filter{ $0.assetCollectionSubtype != .smartAlbumAllHidden }
+        }
+        for assetCollection in _photoAssetCollections {
+            // Add image collection view controllers.
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .vertical
+            let assetViewController = AssetsViewController(collectionViewLayout: flowLayout, photoAlbum: assetCollection)
+            self.addViewController(assetViewController)
+            // _captureDisplayViews.append(assetViewController._captureDisplayView)
+        }
+        didFinishFetchingAssetCollection()
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(colorLiteralRed: 0.976, green: 0.976, blue: 0.976, alpha: 1.0)
+        tabNavigationBar.isTranslucent = true
+        let cancel = TabNavigationItem(title: "取消", target: self, selector: #selector(_handleCancelAction(_:)))
+        tabNavigationBar.navigationItems = [cancel]
+        
+        _captureSession?.startRunning()
+    }
+    
+    deinit {
+        _captureSession?.stopRunning()
+    }
+
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: Private.
+
+extension TabNavigationImagePickerController {
+    fileprivate func _initSession() {
+        _captureSessionQueue.async { [weak self] in
+            guard let wself = self else { return }
+            if let device = type(of: wself).defaultDeviceOfCaptureSessionInputs() {
+                do {
+                    wself._captureDeviceInput = try AVCaptureDeviceInput(device: device)
+                    
+                    wself._captureDisplayOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable: kCVPixelFormatType_32BGRA]
+                    wself._captureDisplayOutput.setSampleBufferDelegate(wself, queue: wself._captureDisplayQueue)
+                    wself._captureDisplayOutput.alwaysDiscardsLateVideoFrames = true
+                    
+                    wself._captureSession = AVCaptureSession()
+                    if wself._captureSession.canSetSessionPreset(AVCaptureSessionPresetPhoto) {
+                        wself._captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+                    }
+                    if wself._captureSession.canAddInput(wself._captureDeviceInput) {
+                        wself._captureSession.addInput(wself._captureDeviceInput)
+                    }
+                    if wself._captureSession.canAddOutput(wself._captureDisplayOutput) {
+                        wself._captureSession.addOutput(wself._captureDisplayOutput)
+                    }
+                    
+                    DispatchQueue.main.async {
+                        wself._captureVideoPreviewView = CameraViewController.CaptureVideoPreviewView(session: wself._captureSession)
+                        wself._captureVideoPreviewView.previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                        wself._captureVideoPreviewView.translatesAutoresizingMaskIntoConstraints = false
+                    }
+                } catch let error {
+                    print("The input for the specific device is not avaiable: \(error)")
+                }
+            } else {
+                print("The current default device of the specific media type is not available.")
+            }
+        }
+    }
+}
+
+// MARK: Actions.
+
+extension TabNavigationImagePickerController {
+    @objc
+    fileprivate func _handleCancelAction(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK:  Public.
+
+extension TabNavigationImagePickerController {
+    open var shouldIncludeHiddenAssets: Bool { return false }
+    
+    public func requestAuthorizationIfNeeded() {
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .authorized: break
+        default:
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                // Handle authorization status.
+                print("PHPhotoLibrary.requestAuthorization: \(status)")
+            })
+        }
+    }
+    
+    open class func generatePhotoAssetCollections() -> PHFetchResult<PHAssetCollection> {
+        let option = PHFetchOptions()
+        option.includeHiddenAssets = true
+        option.includeAllBurstAssets = false
+        return PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .any, options: option)
+    }
+    
+    open class func defaultDeviceOfCaptureSessionInputs() -> AVCaptureDevice? {
+        guard let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo) else { return nil }
+        // Configure the default device.
+        DispatchQueue(label: "com.device.initializer.config").async {
+            do {
+                try device.lockForConfiguration()
+                
+                device.isSubjectAreaChangeMonitoringEnabled = true
+                
+                if device.isFlashModeSupported(.auto) {
+                    device.flashMode = .auto
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+        return device
+    }
+    
+    open func willBeginFetchingAssetCollection() {
+        delegate?.imagePickerWillBeginFetchingAssetCollection?(self)
+    }
+    
+    open func didFinishFetchingAssetCollection() {
+        delegate?.imagePickerDidFinishFetchingAssetCollection?(self)
+    }
+}
+
+extension TabNavigationImagePickerController {
+    fileprivate class var resourceBundlePath: String { return String(describing: self) + ".bundle/" }
+}
+
+// MARK: AVCaptureVideoDataOutputSampleBufferDelegate.
+
+extension TabNavigationImagePickerController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    public func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        print("Count of display views: \(_captureDisplayViews.count)")
+        // _lastSampleBuffer = sampleBuffer
+        guard RunLoop.main.currentMode != .UITrackingRunLoopMode else {
+            self._captureDisplayViews.forEach({ (displayView) in
+                if !displayView.blured {
+                    let image = UIImage.image(from: sampleBuffer)?.lightBlur
+                    displayView.blur(image, duration: 0.05)
+                }
+            })
+            return
+        }
+        autoreleasepool{ _captureDisplayQueue.async { [weak self] in self?._captureDisplayViews.forEach({ if $0.blured { $0.unBlur() }; $0.draw(buffer: sampleBuffer) }) } }
+    }
+}
---
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigations/ImagePicker/Utility.swift
@@ -0,0 +1,254 @@
+//
+//  Utility.swift
+//  TabNavigations/ImagePicker
+//
+//  Created by devedbox on 2017/8/25.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+import Accelerate
+import AVFoundation
+
+public extension UIEdgeInsets {
+    public var width: CGFloat { return left + right }
+    public var height: CGFloat { return top + bottom }
+    public init(width: CGFloat) {
+        self.init(top: 0.0, left: width * 0.5, bottom: 0.0, right: width * 0.5)
+    }
+    public init(height: CGFloat) {
+        self.init(top: height * 0.5, left: 0.0, bottom: height * 0.5, right: 0.0)
+    }
+    public init(left: CGFloat) {
+        self.init(top: 0.0, left: left, bottom: 0.0, right: 0.0)
+    }
+    public init(right: CGFloat) {
+        self.init(top: 0.0, left: 0.0, bottom: 0.0, right: right)
+    }
+    public init(top: CGFloat) {
+        self.init(top: top, left: 0.0, bottom: 0.0, right: 0.0)
+    }
+    public init(bottom: CGFloat) {
+        self.init(top: 0.0, left: 0.0, bottom: bottom, right: 0.0)
+    }
+}
+
+internal extension UIView {
+    func removeConstraintIfNotNil(_ constraint: NSLayoutConstraint?) { if let const_ = constraint { removeConstraint(const_) } }
+}
+
+public extension UIImage {
+    public func blur(radius: CGFloat) -> UIImage? { return _blur(radius: radius, tintColor: nil, saturationDeltaFactor: -1.0, mask: nil) }
+    
+    public var lightBlur: UIImage? { return _blur(radius: 40.0, tintColor: UIColor(white: 1.0, alpha: 0.3), saturationDeltaFactor: 1.8, mask: nil) }
+    public var extraLightBlur: UIImage? { return _blur(radius: 40.0, tintColor: UIColor(white: 0.97, alpha: 0.82), saturationDeltaFactor: 1.8, mask: nil) }
+    public var darkBlur: UIImage? { return _blur(radius: 40.0, tintColor: UIColor(white: 0.11, alpha: 0.73), saturationDeltaFactor: 1.8, mask: nil) }
+    
+    public func blur(tint tintColor: UIColor) -> UIImage? { return _blur(radius: 20.0, tintColor: tintColor.withAlphaComponent(0.6), saturationDeltaFactor: -1.0, mask: nil) }
+    
+    private func _blur(radius: CGFloat, tintColor: UIColor?, saturationDeltaFactor: CGFloat, mask: UIImage?) -> UIImage? {
+        // Check pre-conditions.
+        guard size.width >= 1.0 && size.height >= 1.0 else { return nil }
+        guard let input = cgImage else { return nil }
+        if let _ = mask { guard let _ = mask?.cgImage else { return nil } }
+        
+        let hasBlur = radius > .ulpOfOne
+        let hasSaturationChange = fabs(saturationDeltaFactor - 1.0) > .ulpOfOne
+        
+        let inputScale = scale
+        let inputBitmapInfo = input.bitmapInfo
+        let inputAlphaInfo = CGImageAlphaInfo(rawValue: inputBitmapInfo.intersection([.alphaInfoMask]).rawValue)
+        
+        let outputSizeInPoints = size
+        let outputRectInPoints = CGRect(origin: .zero, size: outputSizeInPoints)
+        
+        // Set up output context.
+        var useOpaqueContext: Bool
+        if inputAlphaInfo == .none || inputAlphaInfo == .noneSkipLast || inputAlphaInfo == .noneSkipFirst {
+            useOpaqueContext = true
+        } else {
+            useOpaqueContext = false
+        }
+        UIGraphicsBeginImageContextWithOptions(outputSizeInPoints, useOpaqueContext, inputScale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.translateBy(x: 0.0, y: -outputSizeInPoints.height)
+        
+        if hasBlur || hasSaturationChange {
+            var effectInBuffer: vImage_Buffer = vImage_Buffer()
+            var scratchBuffer1: vImage_Buffer = vImage_Buffer()
+            var inputBuffer: vImage_Buffer
+            var outputBuffer: vImage_Buffer
+            
+            var format = vImage_CGImageFormat(
+                bitsPerComponent: 8,
+                bitsPerPixel: 32,
+                colorSpace: nil,
+                // (kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little)
+                // requests a BGRA buffer.
+                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue),
+                version: 0,
+                decode: nil,
+                renderingIntent: .defaultIntent)
+            
+            let e = vImageBuffer_InitWithCGImage(&effectInBuffer, &format, nil, input, vImage_Flags(kvImagePrintDiagnosticsToConsole))
+            if e != kvImageNoError {
+                UIGraphicsEndImageContext()
+                return nil
+            }
+            
+            vImageBuffer_Init(&scratchBuffer1, effectInBuffer.height, effectInBuffer.width, format.bitsPerPixel, vImage_Flags(kvImageNoFlags))
+            
+            inputBuffer = effectInBuffer
+            outputBuffer = scratchBuffer1
+            
+            if hasBlur {
+                // A description of how to compute the box kernel width from the Gaussian
+                // radius (aka standard deviation) appears in the SVG spec:
+                // http://www.w3.org/TR/SVG/filters.html#feGaussianBlurElement
+                //
+                // For larger values of 's' (s >= 2.0), an approximation can be used: Three
+                // successive box-blurs build a piece-wise quadratic convolution kernel, which
+                // approximates the Gaussian kernel to within roughly 3%.
+                //
+                // let d = floor(s * 3*sqrt(2*pi)/4 + 0.5)
+                //
+                // ... if d is odd, use three box-blurs of size 'd', centered on the output pixel.
+                //
+                var inputRadius = radius * inputScale
+                if inputRadius - 2.0 < .ulpOfOne { inputRadius = 2.0 }
+                let _tmpRadius = floor((Double(inputRadius) * 3.0 * sqrt(Double.pi * 2.0) / 4.0 + 0.5) / 2.0)
+                let _radius = UInt32(_tmpRadius) | 1 // force radius to be odd so that the three box-blur methodology works.
+                
+                let tempBufferSize = vImageBoxConvolve_ARGB8888(&inputBuffer, &outputBuffer, nil, 0, 0, _radius, _radius, nil, vImage_Flags(kvImageGetTempBufferSize | kvImageEdgeExtend))
+                
+                let tempBuffer = malloc(tempBufferSize)
+                defer { free(tempBuffer) }
+                
+                vImageBoxConvolve_ARGB8888(&inputBuffer, &outputBuffer, tempBuffer, 0, 0, _radius, _radius, nil, vImage_Flags(kvImageEdgeExtend))
+                vImageBoxConvolve_ARGB8888(&outputBuffer, &inputBuffer, tempBuffer, 0, 0, _radius, _radius, nil, vImage_Flags(kvImageEdgeExtend))
+                vImageBoxConvolve_ARGB8888(&inputBuffer, &outputBuffer, tempBuffer, 0, 0, _radius, _radius, nil, vImage_Flags(kvImageEdgeExtend))
+                
+                let tmpBuffer = inputBuffer
+                inputBuffer = outputBuffer
+                outputBuffer = tmpBuffer
+            }
+            
+            if hasSaturationChange {
+                let s = saturationDeltaFactor
+                // These values appear in the W3C Filter Effects spec:
+                // https://dvcs.w3.org/hg/FXTF/raw-file/default/filters/index.html#grayscaleEquivalent
+                //
+                let floatingPointSaturationMatrix: [CGFloat] = [
+                    0.0722 + 0.9278 * s,  0.0722 - 0.0722 * s,  0.0722 - 0.0722 * s,  0.0,
+                    0.7152 - 0.7152 * s,  0.7152 + 0.2848 * s,  0.7152 - 0.7152 * s,  0.0,
+                    0.2126 - 0.2126 * s,  0.2126 - 0.2126 * s,  0.2126 + 0.7873 * s,  0.0,
+                    0.0,                  0.0,                  0.0,                  1.0,
+                    ]
+                let divisor: Int32 = 256
+                // let matrixSize = MemoryLayout.size(ofValue: floatingPointSaturationMatrix) / MemoryLayout.size(ofValue: floatingPointSaturationMatrix[0])
+                // let matrixSize = floatingPointSaturationMatrix.count
+                var saturationMatrix: [Int16] = []
+                for /*i in 0 ..< matrixSize*/ floatingPointSaturation in floatingPointSaturationMatrix {
+                    saturationMatrix.append(Int16(roundf(Float(/*floatingPointSaturationMatrix[i]*/floatingPointSaturation * CGFloat(divisor)))))
+                }
+                vImageMatrixMultiply_ARGB8888(&inputBuffer, &outputBuffer, saturationMatrix, divisor, nil, nil, vImage_Flags(kvImageNoFlags))
+                
+                let tmpBuffer = inputBuffer
+                inputBuffer = outputBuffer
+                outputBuffer = tmpBuffer
+            }
+            
+            func cleanupBuffer(userData: UnsafeMutableRawPointer?, buf_data: UnsafeMutableRawPointer?) {
+                if let buffer = buf_data { free(buffer) }
+            }
+            var effectCGImage = vImageCreateCGImageFromBuffer(&inputBuffer, &format, cleanupBuffer, nil, vImage_Flags(kvImageNoAllocate), nil)
+            if effectCGImage == nil {
+                effectCGImage = vImageCreateCGImageFromBuffer(&inputBuffer, &format, nil, nil, vImage_Flags(kvImageNoFlags), nil)
+                free(inputBuffer.data)
+            }
+            
+            if mask != nil {
+                // Only need to draw the base image if the effect image will be masked.
+                context.__draw(in: outputRectInPoints, image: input)
+            }
+            // draw effect image
+            context.saveGState()
+            if let maskCGImage = mask?.cgImage {
+                context.clip(to: outputRectInPoints, mask: maskCGImage)
+            }
+            if let _cgImage = effectCGImage?.takeUnretainedValue() {
+                context.__draw(in: outputRectInPoints, image: _cgImage)
+            }
+            context.restoreGState()
+            
+            // Cleanup
+            // CGImageRelease(effectCGImage as! CGImage)
+            effectCGImage?.release()
+            free(outputBuffer.data)
+        } else {
+            // draw base image
+            context.__draw(in: outputRectInPoints, image: input)
+        }
+        
+        // Add in color tint.
+        if tintColor != nil {
+            context.saveGState()
+            context.setFillColor(tintColor!.cgColor)
+            context.fill(outputRectInPoints)
+            context.restoreGState()
+        }
+        // Output image is ready.
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
+
+extension UIImage {
+    public class func image(from sampleBuffer: CMSampleBuffer, applying: ((CGSize) -> CGAffineTransform)? = nil) -> UIImage? {
+        // Get a CMSampleBuffer's Core Video image buffer for the media data
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+        // Lock the base address of the pixel buffer
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        // Get the number of bytes per row for the pixel buffer
+        guard let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer) else { return nil }
+        
+        // Get the number of bytes per row for the pixel buffer
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        // Get the pixel buffer width and height
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        
+        // Create a device-dependent RGB color space
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        // Create a bitmap graphics context with the sample buffer data
+        let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        guard let context = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else { return nil }
+        // Create a Quartz image from the pixel data in the bitmap graphics context
+        guard let _originalImage = context.makeImage() else { return nil }
+        // Unlock the pixel buffer
+        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        guard let rotateCtx = CGContext(data: nil, width: height, height: width, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else {return nil }
+        if let transform = applying?(CGSize(width: width, height: height)) {
+            rotateCtx.concatenate(transform)
+        } else {
+            rotateCtx.translateBy(x: 0.0, y: CGFloat(width))
+            rotateCtx.rotate(by: -CGFloat.pi * 0.5)
+        }
+        rotateCtx.draw(_originalImage, in: CGRect(origin: .zero, size: CGSize(width: width, height: height)))
+        guard let _image = rotateCtx.makeImage() else { return nil }
+        
+        // Free up the context and color space
+        // CGContextRelease(context);
+        // CGColorSpaceRelease(colorSpace);
+        
+        // Create an image object from the Quartz image
+        let image = UIImage(cgImage: _image, scale: UIScreen.main.scale, orientation: .up)
+        return image
+    }
+}
