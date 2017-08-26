735d38001ab183b8242d1edc706e2eb8cafc1053
devedbox <862099730@qq.com>
2017-08-26 18:05:11 +0800

    Added errors definitions.

--- AxReminder/Classes/Generals/ViewControllers/TabNavigations/ImagePicker/Errors.swift
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigations/ImagePicker/Errors.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2017年 devedbox. All rights reserved.
 //
 
-
+public enum CaptureVideoPreviewViewError: Error {
+    public enum ConfiguragionError: Error {
+        case noneOfAvailableDevices
+        case sessionCannotAddInput
+    }
+    case configuration(ConfiguragionError)
+}
