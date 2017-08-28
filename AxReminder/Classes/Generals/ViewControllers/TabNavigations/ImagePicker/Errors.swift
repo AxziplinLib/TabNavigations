605f588a72939ffbe70ae0b0772cc3c9bc6149eb
aixing-hb <aixing@hydsoft.com>
2017-08-28 09:40:21 +0800

    Added comments annotations.

--- AxReminder/Classes/Generals/ViewControllers/TabNavigations/ImagePicker/Errors.swift
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigations/ImagePicker/Errors.swift
@@ -6,10 +6,15 @@
 //  Copyright © 2017年 devedbox. All rights reserved.
 //
 
+/// An error type descriping the errors occured in the CaptureVideoPreviewView type.
 public enum CaptureVideoPreviewViewError: Error {
+    /// An error type descriping the errors occured in the Configuragion modules.
     public enum ConfiguragionError: Error {
+        /// Indicates no available camera devices found during the video capture session's configuration.
         case noneOfAvailableDevices
+        /// Indicates the input cannot be added to the video capture session.
         case sessionCannotAddInput
     }
+    /// Indicates the errors occured during session's configuration.
     case configuration(ConfiguragionError)
 }
