86332181b687e94f5e3b2625dbc25ebd02ffe158
aixing-hb <aixing@hydsoft.com>
2017-09-19 10:23:53 +0800

    Added _ScrollViewDidScroll.

--- AxReminder/Classes/Generals/ViewControllers/TabNavigations/_TabNavigationBarScrollViewDelegates.swift
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigations/_TabNavigationBarScrollViewDelegates.swift
@@ -30,3 +30,24 @@ extension _ScrollViewDidEndScrollingAnimation: UIScrollViewDelegate {
         _completion(scrollView)
     }
 }
+
+// MARK: - _ScrollViewDidScroll.
+
+/// A type reresenting the a hook of UIScrollViewDelegate to set as the temporary
+/// delegate and to get the call back when scroll view did scroll.
+internal class _ScrollViewDidScroll: NSObject {
+    fileprivate var _didScroll: ((UIScrollView) -> Void)
+    /// Creates a _ScrollViewDidScroll object with the call back closure.
+    ///
+    /// - Parameter completion: A closure will be triggered when the `scrollViewDidScroll(:)` is called.
+    /// - Returns: A _ScrollViewDidScroll with the call back closure.
+    internal init(_ didScroll: @escaping ((UIScrollView) -> Void)) {
+        _didScroll = didScroll
+    }
+}
+// Confirming of UIScrollViewDelegate.
+extension _ScrollViewDidScroll: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        _didScroll(scrollView)
+    }
+}
