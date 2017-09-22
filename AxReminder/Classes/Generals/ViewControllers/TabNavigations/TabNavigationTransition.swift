6f0c0f1ebee6e200ddd4d5406756bfffcf8cee56
aixing-hb <aixing@hydsoft.com>
2017-09-22 17:19:45 +0800

    Updated the calling of underlying button.

--- AxReminder/Classes/Generals/ViewControllers/TabNavigations/TabNavigationTransition.swift
+++ AxReminder/Classes/Generals/ViewControllers/TabNavigations/TabNavigationTransition.swift
@@ -39,58 +39,57 @@ extension TabNavigationTitleItemTransition {
 extension TabNavigationTitleItemTransition: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard !scrollView.isDragging, !scrollView.isTracking, !scrollView.isDecelerating else { return }
-        guard let titleItems = tabNavigationBar?.navigationTitleItems, let _offsetPositionsUpToEndIndex = tabNavigationBar?._offsetPositionsUpToEndIndex else { return }
-        let offsetX = scrollView.contentOffset.x
+        guard let  titleItems                  = tabNavigationBar?.navigationTitleItems,
+              let _offsetPositionsUpToEndIndex = tabNavigationBar?._offsetPositionsUpToEndIndex
+        else { return }
+        let   offsetx  = scrollView.contentOffset.x
+        // print("Transition offset: \(offsetx)")
+        guard !_offsetPositionsUpToEndIndex.isEmpty && !titleItems.isEmpty else { return }
+        guard offsetx >= 0 && offsetx <= _offsetPositionsUpToEndIndex[titleItems.lastIndex] else { return }
         
-        autoreleasepool { _transitionQueue.async { autoreleasepool { [unowned self] in
-            for (index, titleItem) in titleItems.enumerated() {
-                let offsetPosition      = _offsetPositionsUpToEndIndex[index]
-                let selected            = (offsetPosition >= offsetX)
-                
-                if  selected { guard index > titleItems.startIndex else { continue } } else {
-                    guard index < titleItems.index(before: titleItems.endIndex) else { continue }
-                }
-                
-                let offsetPositionDelta = selected
-                    ? (offsetPosition - _offsetPositionsUpToEndIndex[titleItems.index(before: index)])
-                    : (_offsetPositionsUpToEndIndex[titleItems.index(after: index)] - offsetPosition)
-                
-                guard fabs(offsetPosition - offsetX) <= offsetPositionDelta else {
-                    DispatchQueue.main.async { titleItem.selected = false }; continue
-                }
-                
-                let relativeOffsetX = selected
-                    ? (offsetX - _offsetPositionsUpToEndIndex[titleItems.index(before: index)])
-                    : (offsetX -  offsetPosition)
-                let transitionPercent = relativeOffsetX / offsetPositionDelta
-                
-                let color = UIColor.color(from: titleItem.titleColor(whenSelected: !selected),
-                                          to  : titleItem.titleColor(whenSelected:  selected),
-                                          percent: transitionPercent)
-                let font  = UIFont.font(from: titleItem.titleFont(whenSelected: !selected),
-                                        to  : titleItem.titleFont(whenSelected:  selected),
-                                        percent: transitionPercent)!
-                
-                if  let range     = titleItem.selectedRange {
-                    let _ns_range = NSMakeRange(range.lowerBound, range.upperBound - range.lowerBound)
-                    let
-                    attributedTitle = NSMutableAttributedString(attributedString: titleItem._button.attributedTitle(for: .normal)!)
-                    attributedTitle.addAttributes([NSFontAttributeName: font], range: _ns_range)
-                    attributedTitle.addAttributes([NSForegroundColorAttributeName: color], range: _ns_range)
-                    
-                    DispatchQueue.main.async {
-                        titleItem._button.setAttributedTitle(attributedTitle, for: .normal)
-                    }
-                } else {
-                    DispatchQueue.main.async {
-                        titleItem._button.titleLabel?.font = font
-                        titleItem._button.setTitleColor(color, for: .normal)
-                        titleItem._button.tintColor = color
-                    }
-                }
-                
-                self.didUpdateSelectedIndex?(transitionPercent > 0.5 ? (selected ? index : titleItems.index(after: index)) : (selected ? titleItems.index(before: index) : index))
+        for (index, titleItem) in titleItems.enumerated() {
+            let offsetPosition      = _offsetPositionsUpToEndIndex[index]
+            let selected            = (offsetPosition >= offsetx)
+            
+            if  selected { guard index > titleItems.startIndex else { continue } } else {
+                guard index < titleItems.index(before: titleItems.endIndex) else { continue }
             }
-        } } }
+            
+            let offsetPositionDelta = selected
+                ? (offsetPosition - _offsetPositionsUpToEndIndex[titleItems.index(before: index)])
+                : (_offsetPositionsUpToEndIndex[titleItems.index(after: index)] - offsetPosition)
+            
+            guard fabs(offsetPosition - offsetx) <= offsetPositionDelta else {
+                titleItem.selected = false; continue
+            }
+            
+            let relativeOffsetX = selected
+                ? (offsetx - _offsetPositionsUpToEndIndex[titleItems.index(before: index)])
+                : (offsetx -  offsetPosition)
+            let transitionPercent = relativeOffsetX / offsetPositionDelta
+            
+            let color = UIColor.color(from: titleItem.titleColor(whenSelected: !selected),
+                                      to  : titleItem.titleColor(whenSelected:  selected),
+                                      percent: transitionPercent)
+            let font  = UIFont.font(from: titleItem.titleFont(whenSelected: !selected),
+                                    to  : titleItem.titleFont(whenSelected:  selected),
+                                    percent: transitionPercent)!
+            
+            if  let range     = titleItem.selectedRange {
+                let _ns_range = NSMakeRange(range.lowerBound, range.upperBound - range.lowerBound)
+                let
+                attributedTitle = NSMutableAttributedString(attributedString: titleItem.underlyingButton.attributedTitle(for: .normal)!)
+                attributedTitle.addAttributes([NSFontAttributeName: font], range: _ns_range)
+                attributedTitle.addAttributes([NSForegroundColorAttributeName: color], range: _ns_range)
+                
+                titleItem.underlyingButton.setAttributedTitle(attributedTitle, for: .normal)
+            } else {
+                titleItem.underlyingButton.titleLabel?.font = font
+                titleItem.underlyingButton.setTitleColor(color, for: .normal)
+                titleItem.underlyingButton.tintColor = color
+            }
+            
+            self.didUpdateSelectedIndex?(transitionPercent > 0.5 ? (selected ? index : titleItems.index(after: index)) : (selected ? titleItems.index(before: index) : index))
+        }
     }
 }
