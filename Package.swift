32c6bd73686063880f2ca071998237915128b799
aixing-hb <aixing@hydsoft.com>
2017-09-26 15:55:52 +0800

    Added Package.swift file.

---
+++ Package.swift
@@ -0,0 +1,11 @@
+//
+//  Package.swift
+//  TabNavigations
+//
+//  Created by devedbox on 2017/9/11.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "TabNavigations", dependencies : [], exclude: [])
