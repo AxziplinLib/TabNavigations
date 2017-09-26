0d29fe8621a4f0195c64ba2478595cad1f934269
devedbox <862099730@qq.com>
2017-09-26 19:58:25 +0800

    Renamed and added excludes.

--- Package.swift
+++ Package.swift
@@ -8,4 +8,4 @@
 
 import PackageDescription
 
-let package = Package(name: "TabNavigations", dependencies : [], exclude: [])
+let package = Package(name: "TabNavigations", dependencies : [], exclude: ["Sources/Resources.bundle"])
