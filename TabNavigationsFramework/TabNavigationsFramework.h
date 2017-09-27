3abde06f48e751e637fcb8d6a65edcae513c272f
aixing-hb <aixing@hydsoft.com>
2017-09-27 11:30:01 +0800

    Added Carthage supportings.

---
+++ TabNavigationsFramework/TabNavigationsFramework.h
@@ -0,0 +1,19 @@
+//
+//  TabNavigationsFramework.h
+//  TabNavigationsFramework
+//
+//  Created by devedbox on 2017/9/27.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TabNavigationsFramework.
+FOUNDATION_EXPORT double TabNavigationsFrameworkVersionNumber;
+
+//! Project version string for TabNavigationsFramework.
+FOUNDATION_EXPORT const unsigned char TabNavigationsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TabNavigationsFramework/PublicHeader.h>
+
+
